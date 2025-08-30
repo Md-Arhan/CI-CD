8a214026cf113c4638c7f4c75c4096b22e4ce0cc
Mohammed Arhan <mdarhan0309@gmail.com>
2025-08-30 21:44:59 +0530

    number changes

--- sum.test.js
+++ sum.test.js
@@ -4,7 +4,7 @@ import { sum } from "./sum.js";
 
 
 describe("test for sum fxn", () => {
-    test('adds 2 + 2 to equal 4', () => {
+    test('adds 1 + 2 to equal 3', () => {
       expect(sum(1, 2)).toBe(3);
     });
     // test('adds 2 - 2 to equal 0', () => {
